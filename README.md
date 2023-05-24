fd75640812f4cd4a70643166c85b633dc0b63522
harryscholes <harryscholes@users.noreply.github.com>
2023-05-24 12:01:48 +0300

    Add skeleton app

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # friendly-sniffle
+
+## Setup
+
+```
+conda create -n friendly-sniffle python=3.11
+conda activate friendly-sniffle
+pip install -r requirements.txt
+```
+
+## Testing
+
+```
+pytest
+```
+
+## Running
+
+- Obtain a Covalent API key and set it to `COVALENT_API_KEY` as an environment variable, optionally in a `.env` file.
+- Run:
+
+```
+# Development
+hypercorn app/main:app --reload
+
+# Production
+hypercorn app/main:app
+```
---
+++ app/test_main.py
@@ -0,0 +1,33 @@
+# Simple s
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+wallet_address = "0x6105f0b07341eE41562fd359Ff705a8698Dd3109"
+
+
+def test_get_tokens_for_address():
+    response = client.get(f"/get-tokens-for-address/address/{wallet_address}")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data["tokens"]) > 0
+
+
+def test_get_usd_value_for_address():
+    response = client.get(f"/get-usd-value-for-address/address/{wallet_address}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["usd_value"] > 0.0
+
+
+def test_get_transactions_for_address():
+    response = client.get(f"/get-transactions-for-address/address/{wallet_address}/page/0")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data["transactions"]) > 0
+
+    response = client.get(f"/get-transactions-for-address/address/{wallet_address}/page/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data["transactions"]) > 0
